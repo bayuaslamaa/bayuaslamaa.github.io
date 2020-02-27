6608602f93da4c61cd22d9f5dc055e3597906f0c
bayuaslamaa <bayuaslamaa@gmail.com>
2020-02-27 21:45:29 +0700

    Version 3 Beta.2

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ const bebek = document.querySelectorAll('.bebek');
 const score = document.querySelector('.score')
 const shotgun = document.querySelector('#shotgun')
 const quack = document.querySelector('#quack')
+const hard = document.querySelector('.mulaiHard')
+const easy = document.querySelector('.mulaiEasy')
 
 let highestScore;
 let rumputPrevious;
 let selesai;
 let skor;
 
-// alert('Kamu akan memainkan sebuah game tembektu: tembak bebek itu, klik "Tembak!" untuk memulai')
+alert('Kamu akan memainkan sebuah game tembektu: tembak bebek itu, klik "Tembak!" untuk memulai')
 
 function randomGrass(rumput) {
     const r = Math.floor(Math.random() * rumput.length);
@@ -26,11 +28,24 @@ let randomTime = function (min, max) {
     return Math.ceil(Math.random() * (max - min) + min)
 }
 
+// if (hard) {
+//     let levelHard = function (min, max) {
+//         let min = 400;
+//         let max = 650;
+//         return min, max
+//     }
+// }
+let tRandom;
+
 function showDuck() {
     const rRandom = randomGrass(rumput)
-    const tRandom = randomTime(400, 650);
     rRandom.classList.add('timbul');
-
+    // tRandom = tRandom
+    if (hard) {
+        tRandom = randomTime(400, 650)
+    } else if (easy) {
+        tRandom = randomTime(2000, 3000);
+    }
 
     setTimeout(() => {
         quack.play()
@@ -41,7 +56,8 @@ function showDuck() {
     }, tRandom);
 }
 
-let timeTotal = prompt("Masukan waktu bermain dalam detik:", 5)
+
+let timeTotal = prompt("Masukan waktu bermain dalam detik:", 10)
 
 let startTheGame = function () {
     selesai = false;
@@ -57,8 +73,8 @@ let startTheGame = function () {
 
 let shot = function () {
     shotgun.play();
-    skor++
     this.parentNode.classList.remove('timbul');
+    skor++
     score.textContent = skor;
 
 }
