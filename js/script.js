49a5a45329c3f3bd84252632ecb123e261d0d4c8
bayuaslamaa <bayuaslamaa@gmail.com>
2020-02-27 11:21:11 +0700

    buat program dlm javascript dan edit UI

--- js/script.js
+++ js/script.js
@@ -1,15 +1,53 @@
 const rumput = document.querySelectorAll('.rumput');
 const bebek = document.querySelectorAll('.bebek');
+const score = document.querySelector('.score')
 
-let rumputPrevios;
+let rumputPrevious;
+let selesai;
+let skor;
+
 
 function randomGrass(rumput) {
-    const rRandom = Math.floor(Math.random() * rumput.length);
-    return rumput[rRandom];
+    const r = Math.floor(Math.random() * rumput.length);
+    const rRandom = rumput[r]
+    if (rRandom == rumputPrevious) {
+        randomGrass(rumput)
+    }
+    rumputPrevious = rRandom
+    return rRandom;
+
 }
 
-function showDuck(rumput) {
+let randomTime = function (min, max) {
+    return Math.ceil(Math.random() * (max - min) + min)
+}
+
+function showDuck() {
     const rRandom = randomGrass(rumput)
+    const tRandom = randomTime(100, 600);
     rRandom.classList.add('timbul');
 
+    setTimeout(() => {
+        rRandom.classList.remove('timbul');
+        showDuck();
+    }, tRandom);
 }
+
+let startTheGame = function () {
+    selesai = false;
+    skor = 0;
+    showDuck()
+    setTimeout(() => {
+        selesai = true;
+    }, 15000)
+}
+
+let shot = function () {
+    skor++
+    this.parentNode.classList.remove('timbul');
+    score.textContent = skor;
+}
+
+bebek.forEach(b => {
+    b.addEventListener('click', shot);
+})
