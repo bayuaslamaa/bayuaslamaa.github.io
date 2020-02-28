511e70a79970276bc8e40ca7aa240b5c0e50332b
bayuaslamaa <bayuaslamaa@gmail.com>
2020-02-28 09:08:46 +0700

    versi3 hampir siap deploy

--- js/script.js
+++ js/script.js
@@ -6,12 +6,11 @@ const quack = document.querySelector('#quack')
 const hard = document.querySelector('.mulaiHard')
 const easy = document.querySelector('.mulaiEasy')
 
-let highestScore;
 let rumputPrevious;
 let selesai;
 let skor;
 
-alert('Kamu akan memainkan sebuah game tembektu: tembak bebek itu, klik "Tembak!" untuk memulai')
+alert('Kamu akan memainkan sebuah game tembektu: tembak bebek itu, klik "Start!" untuk memulai')
 
 function randomGrass(rumput) {
     const r = Math.floor(Math.random() * rumput.length);
@@ -28,23 +27,16 @@ let randomTime = function (min, max) {
     return Math.ceil(Math.random() * (max - min) + min)
 }
 
-// if (hard) {
-//     let levelHard = function (min, max) {
-//         let min = 400;
-//         let max = 650;
-//         return min, max
-//     }
-// }
 let tRandom;
 
 function showDuck() {
     const rRandom = randomGrass(rumput)
     rRandom.classList.add('timbul');
-    // tRandom = tRandom
+
     if (hard) {
         tRandom = randomTime(400, 650)
     } else if (easy) {
-        tRandom = randomTime(2000, 3000);
+        tRandom = randomTime(1500, 2100);
     }
 
     setTimeout(() => {
@@ -57,7 +49,7 @@ function showDuck() {
 }
 
 
-let timeTotal = prompt("Masukan waktu bermain dalam detik:", 10)
+let timeTotal = prompt("Masukan waktu bermain dalam detik:", 15)
 
 let startTheGame = function () {
     selesai = false;
@@ -66,11 +58,20 @@ let startTheGame = function () {
     showDuck()
     setTimeout(() => {
         selesai = true;
-        alert(`SELAMAT SCORE ANDA ${score.textContent}`)
+        alert(`SCORE ANDA ${score.textContent}`)
+        if (score.textContent > 10 && easy && timeTotal === 15) {
+            if (window.confirm("Lanjut Level Hard?")) {
+                window.open("hard.html", "_self")
+            }
+        } else {
+            if (score.textContent >= 5 && timeTotal === 15) {
+                window.alert("Selamat, kamu layak berburu!")
+            }
+        }
+
 
     }, timeTotal * 1000)
 }
-
 let shot = function () {
     shotgun.play();
     this.parentNode.classList.remove('timbul');
@@ -82,3 +83,22 @@ let shot = function () {
 bebek.forEach(b => {
     b.addEventListener('click', shot);
 })
+
+function startTimer(duration, display) {
+    let timer = duration,
+        seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = 0 + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    display = document.querySelector('#time');
+    startTimer(timeTotal, display);
+};
